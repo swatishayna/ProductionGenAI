aad24597a841329fe25e9e18ee5d30ee7318fcb1
swatishayna <swati308mmu@gmail.com>
2026-06-11 21:16:11 +0530

    Initial root directory setup

--- src/main.py
+++ src/main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from production-genai!")
+import os
+from contextlib import asynccontextmanager
 
+from fastapi import FastAPI
+import uvicorn
+
+
+@asynccontextmanager
+def lifespan(app: FastAPI):
+    """asynccontextmanager is a decorator from Python's contextlib module 
+    that converts an async generator function into an asynchronous 
+    context manager. Code before yield is executed when entering the context 
+    (setup/startup), and code after yield is executed when exiting the context 
+    (cleanup/shutdown). FastAPI uses it in the lifespan function to initialize
+    shared resources such as databases, vector stores, 
+    and LLM clients during startup and release them during application shutdown."""
+    
+    print("Start")
+    yield "Do Something"
+    print("Cleanup")
+    
+app = FastAPI(
+    title = "Research Paper Curator System",
+    description="Production RAG for Research Paper",
+    version=os.getenv("APP_VERSION", "0.1.0"),
+    root_path = "/api/v1",
+    lifespan = lifespan,
+)
+# The lifespan parameter in FastAPI is used to run code when your application:
+# Starts up 🚀
+# Shuts down 🛑
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, port=8000, host = "0.0.0.0")
